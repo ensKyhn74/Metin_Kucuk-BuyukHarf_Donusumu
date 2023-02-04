575cb2da24ff2e8e4c1b3df49252bcc93e703439
ensKyhn74 <117343032+ensKyhn74@users.noreply.github.com>
2023-02-04 23:42:45 +0300

    Add files via upload

---
+++ kucuk-buyuk_harf_donusumu.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+ // girilen metindeki küçük harfleri büyüğe,büyük harfleri küçüğe çevir
+
+int main()
+   
+{
+
+ char metin[50];
+ int i = 0;
+ 
+ printf("metin girin: ");
+ gets(metin);
+ 
+ while(metin[i] != '\0')
+ {
+  char ch = metin[i];
+  
+  if(ch >= 65 && ch <= 90)
+  {
+   metin[i] = tolower(metin[i]);
+  }
+  
+  else if(ch >= 97 && ch <= 122)
+  {
+    metin[i] = toupper(metin[i]);
+  }
+  
+  i++;
+ }
+ 
+ printf("%s",metin);
+
+ return 0;
+ 
+}
